4d35d718e1516cf9ea492b120f32590269bd1d11
sivaram0608 <sivaram06082004@gmail.com>
2025-01-28 10:18:29 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,9 @@
+public class basic{
+    public static void main(String[] args)
+{
+    int certificate =100;
+    String bala = "c";
+    String grade = bala;
+    System.out.println(certificate);
+    System.out.println(grade);
+}}
