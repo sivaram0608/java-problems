890be99279c8d0d068fae84d482cf704e794ecce
sivaram0608 <sivaram06082004@gmail.com>
2025-01-28 15:41:48 +0530

    Add files via upload

---
+++ code4.java
@@ -0,0 +1,29 @@
+public class code4 {
+
+    public static void main(String[] args) {
+        int a=10;
+        long b= 100001;
+        float c = 10.5f;
+        double d= 20.99;
+        char e= 'a';
+        boolean i= true;
+        String message ="hello there";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(i);
+        System.out.println(message);
+        int age= a;
+        long distance = b;
+        short year = 2025;
+        float price = c;
+        System.out.println("Age:"+age);
+        System.out.println("distance"+distance);
+        System.out.println("year:"+year);
+        System.out.println("price:"+price);
+
+
+    }
+}
