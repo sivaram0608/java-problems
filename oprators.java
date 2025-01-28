4cc62798a4e044febab4817b5a5f4c1e145c505a
sivaram0608 <sivaram06082004@gmail.com>
2025-01-28 11:51:33 +0530

    Add files via upload

---
+++ oprators.java
@@ -0,0 +1,16 @@
+public class oprators {
+    public static void main(String[] args) {
+        int a = 43;
+        int b = 32;
+        int c = 12;
+        int d = a+c;
+        int e = b-c;
+        int f = a*c;
+        float g = (float)b/c;
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+    
+}
